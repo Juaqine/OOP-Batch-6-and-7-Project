c63f7b6344e769aa86c074faf169628cf52d0849
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:50:17 +0800

    Update capitalize_first_leter.py

--- capitalize_first_leter.py
+++ capitalize_first_leter.py
@@ -1,16 +1,16 @@
-#rog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
-text = input("Enter text: ")
+text = input("Enter a string: ")
 
 if text:
-    first_char = text[0]  # Get the first character
+    first_char = text[0]
     rest = text[1:]
-
-if 'a' <= first_char <= 'z':
+    
+    if 'a' <= first_char <= 'z':
         first_char = chr(ord(first_char) - 32)
 
-new_text = first_char + "".join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in rest)
+    new_text = first_char + "".join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in rest)
 
-print("Output:", new_text)
+    print("Output:", new_text)
 else:
     print("Output: (empty string)")
