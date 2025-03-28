d755aba1a914c7a00756c71b677bd1f7c6194682
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 19:52:03 +0800

    def text

--- convert_lowercase.py
+++ convert_lowercase.py
@@ -1,1 +1,3 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+text = input("Enter text: ")
