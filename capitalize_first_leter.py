e57f0f3a4a103f2d5f5e21d10702bc7c3ae7d326
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:48:33 +0800

    print output

--- capitalize_first_leter.py
+++ capitalize_first_leter.py
@@ -10,3 +10,7 @@ if 'a' <= first_char <= 'z':
         first_char = chr(ord(first_char) - 32)
 
 new_text = first_char + "".join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in rest)
+
+print("Output:", new_text)
+else:
+    print("Output: (empty string)")
