7af4ff283c8eefade00665f3771bf01e3de6b785
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 22:05:48 +0800

    Prog10 Pseudocode

--- title_case.py
+++ title_case.py
@@ -1,4 +1,9 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+#Ask the user for input
+#Split the text
+#Make first character capitalized
+#Make rest of the characters lowercase
+#Print output
 
 text = input("Enter text: ")
 
