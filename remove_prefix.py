2ec02a330770dee5dbe0729401cf4b47d5aca0cd
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 19:47:44 +0800

    Prog02 Pseudocode

--- remove_prefix.py
+++ remove_prefix.py
@@ -1,3 +1,9 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#Ask the user for input
+#Check if text starts with prefix
+#If yes remove prefix
+#Print output
+
 text = input("Enter text: ")
 prefix = input("Enter prefix to remove: ")
 
