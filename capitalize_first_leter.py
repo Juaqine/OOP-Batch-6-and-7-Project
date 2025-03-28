85f962cd060dc1853ffb4c5ea9d62133b1f808e9
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:46:35 +0800

    def text

--- capitalize_first_leter.py
+++ capitalize_first_leter.py
@@ -1,2 +1,3 @@
 #rog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
+text = input("Enter text: ")
