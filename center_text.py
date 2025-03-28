f9877e0a95e3563b53261e2f11a68275b0447133
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:24:16 +0800

    spaces

--- center_text.py
+++ center_text.py
@@ -5,3 +5,8 @@ space = int(input("Enter the total width: "))
 
 if len(text) >= width:
     result = text
+else:
+    total_spaces = width - len(text)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    result = " " * left_spaces + text + " " * right_spaces
