b95d9bdbac769d26f3984f18b30090204ea4571d
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 20:58:52 +0800

    def suffix

--- check_ends_with.py
+++ check_ends_with.py
@@ -1,3 +1,5 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 text = input("Enter text: ")
+suffix = input("Enter suffix to check: ")
+
