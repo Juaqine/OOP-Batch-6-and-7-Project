59995fce7c8988cf148e4b462386e86559ae92e8
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:47:31 +0800

    get first and rest

--- capitalize_first_leter.py
+++ capitalize_first_leter.py
@@ -1,3 +1,7 @@
 #rog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 text = input("Enter text: ")
+
+if text:
+    first_char = text[0]  # Get the first character
+    rest = text[1:]
