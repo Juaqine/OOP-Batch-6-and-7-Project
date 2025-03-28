ddb21f1a3f7f8836bbcd3925874bf84544799511
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:20:34 +0800

    Prog06 Pseudocode

--- add_space.py
+++ add_space.py
@@ -1,4 +1,4 @@
-#ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 #Ask the user for input
 #If text is equal or longer no need to add space
 #Else Add space
