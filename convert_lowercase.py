a9e85393072b809084bad5c247f9c81d08a46aec
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 19:57:57 +0800

    Prog03 Pseudocode

--- convert_lowercase.py
+++ convert_lowercase.py
@@ -1,4 +1,9 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+#Ask the user for input
+#Convert uppercase letters to lowercase
+#If letter uppercase convert to lowercase
+#Else keep letter lowercase
+#Print output
 
 text = input("Enter text: ")
 
