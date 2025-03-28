d21b01c43321affc97aa9d5dcd6979f329cc7efe
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:30:33 +0800

    Prog07 Pseudocode

--- center_text.py
+++ center_text.py
@@ -4,8 +4,8 @@
 #Else Add space both sides
 #Print output
 
-text = input("Enter text:")
-space = int(input("Enter the total width: "))
+text = input("Enter the text: ")
+width = int(input("Enter the total width: "))
 
 if len(text) >= width:
     result = text
@@ -15,4 +15,4 @@ else:
     right_spaces = total_spaces - left_spaces
     result = " " * left_spaces + text + " " * right_spaces
 
-print("Output:", f'"{result}"')
+print("Output:", f'"{result}"') 
