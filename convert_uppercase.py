902556dd10fbb205ffb4243b08a7b44d6c5783fc
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 20:09:58 +0800

    def uppercase

--- convert_uppercase.py
+++ convert_uppercase.py
@@ -1,3 +1,5 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 text = input("Enter text: ")
+
+uppercase_text = ""
