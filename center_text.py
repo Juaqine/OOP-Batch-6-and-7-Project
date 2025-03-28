f4f31a49f02ada2ff4280d74dca7cd2755d8a208
Juaqine <juaqineynicoavila@gmail.com>
2025-03-28 21:23:18 +0800

    def space

--- center_text.py
+++ center_text.py
@@ -1,3 +1,4 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 text = input("Enter text:")
+space = int(input("Enter the total width: "))
