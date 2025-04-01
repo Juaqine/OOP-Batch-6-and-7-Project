3785d70c567596a67119fa020191494f01a2adbf
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:58:55 +0800

    convert lowercase

--- Batch 7/lowercase_alternative.py
+++ Batch 7/lowercase_alternative.py
@@ -1,3 +1,5 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 text = input("Enter text: ")
+
+lower = all('a' <= c <= 'z' for c in text if c.isalpha())
