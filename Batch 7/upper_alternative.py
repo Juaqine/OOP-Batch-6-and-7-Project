dbe89f7020dd8d6fb6e08ea78a20f5fb0a2251a0
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:45:58 +0800

    def text

--- Batch 7/upper_alternative.py
+++ Batch 7/upper_alternative.py
@@ -1,1 +1,3 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+text = input("Enter text: ")
