389fdf5c237096f861e86e9a7bcd5d65543a984d
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:47:14 +0800

    convert uppercase

--- Batch 7/upper_alternative.py
+++ Batch 7/upper_alternative.py
@@ -1,3 +1,7 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 text = input("Enter text: ")
+
+upper_text = "".join(chr(ord(c) - 32) 
+  if 'a' <= c <= 'z' 
+  else c for c in text)
