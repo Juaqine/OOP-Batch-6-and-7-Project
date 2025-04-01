8a7f9950d74f86b04d9902eb6d39d6e8e88fd91d
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 01:22:35 +0800

    Prog08 Pseudocode

--- Batch 7/count.py
+++ Batch 7/count.py
@@ -1,5 +1,9 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
+#Ask the user for input
+#Count the charcters input
+#Print output
+
 text = input("Enter text: ")
 char = input("Enter the character to count: ")
 
