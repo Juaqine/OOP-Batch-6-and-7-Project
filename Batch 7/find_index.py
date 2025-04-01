3fcdfb0ef278a9d7871f22727c2ee17389949e23
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 01:31:02 +0800

    def character

--- Batch 7/find_index.py
+++ Batch 7/find_index.py
@@ -1,3 +1,4 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 text = input("Enter your text: ")
+char = input("Enter the character to find: ")
