36fd80091f60ed0be803e5a8da0001b42099ab19
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:32:33 +0800

    def suffix

--- Batch 7/remove_suffix.py
+++ Batch 7/remove_suffix.py
@@ -1,3 +1,4 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 text = input("Enter a string: ")
+suffix = input("Enter suffix to remove: ")
