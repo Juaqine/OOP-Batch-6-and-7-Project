e8838ac6d4e69729dd3e6ac36faf136aa4a7a638
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 01:51:36 +0800

    Prog10 Pseudocode

--- Batch 7/find_last_index.py
+++ Batch 7/find_last_index.py
@@ -1,5 +1,9 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
+#Ask the user for input
+#Find reverse index
+#Print output
+
 text = input("Enter text: ")
 char = input("Enter the character to find: ")
 
