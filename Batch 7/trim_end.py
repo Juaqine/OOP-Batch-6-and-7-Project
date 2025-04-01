b71368603b7bfe159d3ad0e6ee6e4ad6a0b411c2
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:25:38 +0800

    def text

--- Batch 7/trim_end.py
+++ Batch 7/trim_end.py
@@ -1,1 +1,3 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+text = input("Enter a string: ")
