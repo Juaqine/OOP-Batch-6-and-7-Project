2bf9e19f303676538ebf5b3398fa6121687c7f74
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 00:22:45 +0800

    Prog05 Pseudocode

--- Batch 7/check_prefix.py
+++ Batch 7/check_prefix.py
@@ -1,5 +1,9 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
+#Ask the user for input
+#Check if prefix matches
+#Print output
+
 text = input("Enter text: ")
 prefix = input("Enter prefix to check: ")
 
