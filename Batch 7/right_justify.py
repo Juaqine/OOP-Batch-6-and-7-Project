ebbc4680d375d69f6bd177cfa1d21df00ccb1bd5
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 00:47:21 +0800

    Prog06 Pseudocode

--- Batch 7/right_justify.py
+++ Batch 7/right_justify.py
@@ -1,5 +1,9 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
+#Ask the user for input
+#Add the amount of spaces
+#Print output
+
 text = input("Enter text: ")
 width = int(input("Enter the desired width: "))
 
