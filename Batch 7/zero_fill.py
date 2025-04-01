d0a6f41191844c59e396a0e232424f4cdf6a79d1
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 01:04:14 +0800

    Prog07 Pseudocode

--- Batch 7/zero_fill.py
+++ Batch 7/zero_fill.py
@@ -1,5 +1,9 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
+#Ask the user for input
+#Add the amount of zeros
+#Print output
+
 text = input("Enter text: ")
 width = int(input("Enter the desired width: "))
 
