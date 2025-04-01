65be04312c73e2e6bca43cf87bd6d599e48d91ab
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 00:44:01 +0800

    def spaces

--- Batch 7/right_justify.py
+++ Batch 7/right_justify.py
@@ -1,3 +1,4 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 text = input("Enter text: ")
+width = int(input("Enter the desired width: "))
