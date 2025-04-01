ef61d26cbe218e2edd153e60918303c8b2965707
Juaqine <juaqineynicoavila@gmail.com>
2025-04-02 01:36:56 +0800

    Prog09 Pseudocode

--- Batch 7/find_index.py
+++ Batch 7/find_index.py
@@ -1,5 +1,9 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
+#Ask the user for input
+#Find index
+#Print output
+
 text = input("Enter your text: ")
 char = input("Enter the character to find: ")
 
