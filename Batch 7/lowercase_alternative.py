7e2e636e8c1ec1cfcc0837f7df5e96b4b8ab49b3
Juaqine <juaqineynicoavila@gmail.com>
2025-04-01 23:58:13 +0800

    def text

--- Batch 7/lowercase_alternative.py
+++ Batch 7/lowercase_alternative.py
@@ -1,1 +1,3 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+text = input("Enter text: ")
